fa443fe2284e7956a3449c5ef313fd0841563436
Taylor Allred <taylor.allred21@gmail.com>
2022-09-23 12:55:43 -0600

    outfitted sgesv with tracked floats (need to fix)

--- example/demmel-arxiv/sgesv.jl
+++ example/demmel-arxiv/sgesv.jl
@@ -1,20 +1,20 @@
-include("../src/FloatTracker.jl")
+include("../../src/FloatTracker.jl")
 using .FloatTracker: TrackedFloat64, write_log_to_file
 using LinearAlgebra
 
 # from Demmel section 2.4.1
 # TODO find equivalent
 
-A = [Float64(1) Float64(0);
-     Float64(NaN) Float64(2)]
+A = [TrackedFloat64(1) TrackedFloat64(0);
+     TrackedFloat64(NaN) TrackedFloat64(2)]
 
-b = [Float64(0), Float64(1)]
+b = [TrackedFloat64(0), TrackedFloat64(1)]
 
 ul = 'U' # 'U' = upper tri, 'L' = lower tri
 tA = 'N' # transpose? N = no, T = yes, C = yes and conjugate
 dA = 'U' # N = read, U = ignored
 
 # UHOH what to do
-#println(LinearAlgebra.BLAS.trsv(ul, tA, dA, A, b))
+println(LinearAlgebra.BLAS.trsv(ul, tA, dA, A, b))
 #write_log_to_file()
 
