5efd09908f611ba6a04fded09820f6e57d64c248
Ashton Wiersdorf <github@wiersdorf.dev>
2023-03-17 17:21:44 -0600

    Move new shallow waters examples to examples repo

--- example/shallow-waters/sw_nonan_notf.jl
+++
@@ -1,9 +0,0 @@
-using ShallowWaters, PyPlot
-
-P = run_model(Ndays=100,nx=100,L_ratio=1,bc="nonperiodic",wind_forcing_x="double_gyre",topography="seamount")
-pcolormesh(P.η')
-savefig("height_nonan_notf.png")
-
-speed = sqrt.(Ix(P.u.^2)[:,2:end-1] + Iy(P.v.^2)[2:end-1,:])
-pcolormesh(speed')
-savefig("speed_nonan_notf.png")
